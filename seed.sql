07ad0d21e0b66b3c3927b8f00dfe0354e4cfd84e
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-02-12 09:11:27 -0500

    basic seed added

---
+++ seed.sql
@@ -0,0 +1,12 @@
+/* Seeds for SQL table. */
+USE languagesDB;
+
+/* Populating with Test Data */
+INSERT INTO korea(phrase)
+VALUES ("Hello"), ("Goodbye"), ("Thank You"), ("Excuse Me"), ("May I Have ..."), ("How much does it cost?"), ("Bathroom"), ("Yes/No"), ("I am allergic to ..."), ("Where is ..."), ("Help"), ("Sorry");
+
+INSERT INTO china(phrase)
+VALUES ("Hello"), ("Goodbye"), ("Thank You"), ("Excuse Me"), ("May I Have ..."), ("How much does it cost?"), ("Bathroom"), ("Yes/No"), ("I am allergic to ..."), ("Where is ..."), ("Help"), ("Sorry");
+
+INSERT INTO russia(phrase)
+VALUES ("Hello"), ("Goodbye"), ("Thank You"), ("Excuse Me"), ("May I Have ..."), ("How much does it cost?"), ("Bathroom"), ("Yes/No"), ("I am allergic to ..."), ("Where is ..."), ("Help"), ("Sorry");
