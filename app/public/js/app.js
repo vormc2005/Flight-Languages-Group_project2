974d9cf6e07093483bccc7986d1d5a08a5a28945
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-02-12 13:36:03 -0500

    values added for airports

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -2,12 +2,12 @@
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/?destination=ICN&origin=IAD",
+	"url": "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?destination=ICN&origin=DCA&currency=USD&page=1",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-    "x-rapidapi-key": "aa3dd9aaf7msh72e32624a82ff64p19458djsnf620a5c313c0",
-    "x-access-token": "67284ded587b515fc873b2264829b953"
+		"x-rapidapi-key": "aa3dd9aaf7msh72e32624a82ff64p19458djsnf620a5c313c0",
+		"x-access-token": "67284ded587b515fc873b2264829b953"
 	}
 }
 function flight(){
@@ -16,10 +16,3 @@ $.ajax(settings).done(function (response) {
 });
 }
 flight()
-
-// dropdown javascript
-var dropdown = document.querySelector('.dropdown');
-dropdown.addEventListener('click', function(event) {
-  event.stopPropagation();
-  dropdown.classList.toggle('is-active');
-});
