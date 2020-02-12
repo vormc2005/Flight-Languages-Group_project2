b9b50797b53f43c9f559b22b8dcd225bc89c145c
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-12 15:29:04 -0500

    changed the table columns to english phrases

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -14,9 +14,9 @@ module.exports = function(app) {
   // GET route for getting all of the todos
   app.get("/api/flights", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbLang) {
+    db.Phrase.findAll({}).then(function(dbPhrase) {
       // We have access to the todos as an argument inside of the callback function
-      res.json(dbLang);
+      res.json(dbPhrase);
     });
   });
 
@@ -26,13 +26,23 @@ module.exports = function(app) {
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
-    db.Language.create({
-      phrase: req.body.phrase,
-      inLanguage: req.body.inLanguage,
-      pronunciation: req.body.pronunciation
-    }).then(function(dbLang) {
+    db.Phrase.create({
+      countryname: req.body.countryname,
+      hello: req.body.hello,
+      goodbye: req.body.goodbye,
+      thankyou: req.body.thankyou,
+      excuseme: req.body.excuseme,
+      mayi: req.body.mayi,
+      howmuch: req.body.howmuch,
+      bathroom: req.body.bathroom,
+      yesno: req.body.yesno,
+      allergy: req.body.allergy,
+      whereis: req.body.whereis,
+      help: req.body.help,
+      sorry: req.body.sorry
+    }).then(function(dbPhrase) {
       // We have access to the new todo as an argument inside of the callback function
-      res.json(dbLang);
+      res.json(dbPhrase);
     });
   });
 
