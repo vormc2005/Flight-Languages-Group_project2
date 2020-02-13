6be479067672c6513044bf2bc3ffa7c189cd2c1a
D V <voronov.dmitry@gmail.com>
2020-02-12 22:16:03 -0500

    added russian

--- schema.sql
+++ schema.sql
@@ -1,30 +1,9 @@
 /**Creating DUmmy table***************************
 ***********************************************/
 
--- Drops the day_planner_db if it already exists --
-DROP DATABASE IF EXISTS languages;
+-- Drops the db if it already exists --
+DROP DATABASE IF EXISTS languagesDB;
 
--- Create the database day_planner_db and specified it for use.
-CREATE DATABASE languages;
+-- Create the database db and specified it for use.
+CREATE DATABASE languagesDB;
 
-USE languages;
-
--- Create the table plans.
-CREATE TABLE phrases (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  english varchar(255) NOT NULL,
-  translated varchar(255) NOT NULL,
-  country_code integer(3) NOT NULL,
-  code varchar (4) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE country_codes(
-id integer (3) NOT NULL,
- code  varchar (4) NOT NULL, 
- name varchar(30) NOT NULL,
-    PRIMARY KEY (id)
-);
-
--- Insert a set of records.
-INSERT INTO phrases (english, translated, country_id) VALUES ("Hello", "Privet", 7);
