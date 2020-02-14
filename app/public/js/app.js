48827a9d5a68d33888041ee1de655de18dd7f84b
D V <voronov.dmitry@gmail.com>
2020-02-14 18:11:40 -0500

    Handlebars are done

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,8 +1,10 @@
 // For testing api
-
+console.log('app.js loaded');
+//Function that calls AJAX, using drop down vals as start and destination, then displays ticket price
 function flight() {
   let start = $("#start :selected").val();
   let end = $("#end :selected").val();
+  
   $.ajax({
     url: `https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?destination=${end}&origin=${start}&currency=USD&page=1`,
     method: "GET",
@@ -13,30 +15,32 @@ function flight() {
       "x-access-token": "67284ded587b515fc873b2264829b953"
     }
   }).then(function(response) {
-    let search = response.data;
-    console.log(search);
-    console.log(end);
+    // console.log(end);
+    //checking what the destination value is and then printing the response price
+
 
     switch (end) {
       case "PEK":
-        console.log("china");
         console.log(response.data.BJS[1].price);
         $("#price").text("$" + response.data.BJS[1].price);
+       displayTablevalue();
         break;
       case "ICN":
-        console.log("korea");
         console.log(response.data.SEL[0].price);
         $("#price").text("$" + response.data.SEL[0].price);
+        displayTablevalue();
         break;
       case "DME":
-        console.log("russia");
         console.log(response.data.MOW[0].price);
         $("#price").text("$" + response.data.MOW[0].price);
+        displayTablevalue();
         break;
     }
+    $("#loading").addClass("is-hidden");
+    $("#phrases").removeClass("is-hidden");
+    $("#avrg").removeClass("is-hidden");
   });
 }
-
 $("#add-new").on("click", function(event) {
   console.log("clicked");
   event.preventDefault();
@@ -81,28 +85,49 @@ $("#add-new").on("click", function(event) {
       .val()
       .trim()
   };
-  console.log(newCountry);
+ console.log(newCountry);
+  $.post("/api/flights", newCountry)
+   .then(function(data){
+    console.log(data); 
+  })
 });
-
 //  Show phrases on click
 $("#go").on("click", function() {
   $("#airport").addClass("is-hidden");
-  $("#phrases").removeClass("is-hidden");
-  $("#avrg").removeClass("is-hidden");
-  //   GetSelectedStart();
+  $("#loading").removeClass("is-hidden");
+  // setTimeout(flight, 2000);
   flight();
-  //   showPhrase();
 });
+
+
+displayTablevalue =()=>{
+let countryname = $("#end :selected").text();
+  let countryLower = countryname.toLowerCase();
+    // window.location.href = `http://localhost:8080/${countryLower}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Dmitry
 // for (i=0; i< Phrase.length; i++){
 // 	if (Phrase.countryname ==="Korea"){
 // 		var country = val(phrase.countryname);
 // 		$("#country").append(country)
-
 // 	}
 // }
-
 // $("#end").on("click", function(e) {
 // 	e.preventDefault();
 // 	let end = $("#end :selected").text();
@@ -115,14 +140,12 @@ $("#go").on("click", function() {
 // 			}
 // 		});
 // 			console.log(response);
-
 // 		}else if(end==="Korea"){
 // 			console.log("Lets go to korea")
 // 	}else{
 // 		console.log("Lets go to Russia")
 // 	}
 //   });
-
 // // Khalil
 // function GetSelectedEnd(end){
 // 	// let end = $("#end :selected").val();
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -12,18 +12,19 @@ const db = require("../models");
 // =============================================================
 module.exports = function(app) {
   // GET route for getting all of the todos
-  app.get("/api/flights/:countryname", function(req, res) {
+  app.get("/:countryname", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Phrase.findAll({
+    db.Phrase.findOne({
       where: { countryname: req.params.countryname }
     }).then(function(dbPhrase) {
-      // We have access to the todos as an argument inside of the callback function
-      res.json(dbPhrase);
+      // We have access to the phrases in the table as an argument inside of the callback function
+      console.log(dbPhrase);
+      res.render("index", {phrases: dbPhrase});
     });
   });
 
   // POST route for saving a new todo
-  app.post("/api/flights", function(req, res) {
+  app.post("/", function(req, res) {
     console.log(req.body);
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
@@ -50,8 +51,8 @@ module.exports = function(app) {
 
   // DELETE route for deleting todos. We can get the id of the todo we want to delete from
   // req.params.id
-  app.delete("/api/flights/:id", function(req, res) {});
+  app.delete("/:id", function(req, res) {});
 
   // PUT route for updating flights. We can get the updated todo from req.body
-  app.put("/api/flights", function(req, res) {});
+  app.put("/", function(req, res) {});
 };
