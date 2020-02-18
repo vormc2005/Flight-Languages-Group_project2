0071a64770948859eb16b828317385b00ea83f6a
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-18 11:38:10 -0500

    cleaned up code

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,9 +1,7 @@
-
 // For testing api
-console.log('app.js loaded');
+console.log("app.js loaded");
 
-//Function that calls AJAX, using drop down vals as start and destination, then displays ticket price
-
+//Function that calls AJAX, using drop down values as point of origin and destination, then displays ticket price
 function flight() {
   let start = $("#start :selected").val();
   let end = $("#end :selected").val();
@@ -17,7 +15,6 @@ function flight() {
       "x-access-token": "67284ded587b515fc873b2264829b953"
     }
   }).then(function(response) {
-    // console.log(end);
     //checking what the destination value is and then printing the response price
     switch (end) {
       case "PEK":
@@ -83,76 +80,42 @@ $("#add-new").on("click", function(event) {
       .val()
       .trim()
   };
- console.log(newCountry);
-  $.post("/api/flights", newCountry)
-   .then(function(data){
-    console.log(data); 
-  })
-
-  
+  console.log(newCountry);
+  $.post("/api/flights", newCountry).then(function(data) {
+    console.log(data);
+  });
 });
 
 //  Show phrases on click
 $("#go").on("click", function() {
   $("#airport").addClass("is-hidden");
+  // Shows loading plane buffer on click for two seconds, then runs flight function
   $("#loading").removeClass("is-hidden");
   setTimeout(flight, 2000);
 });
 
-// Dmitry
-// for (i=0; i< Phrase.length; i++){
-// 	if (Phrase.countryname ==="Korea"){
-// 		var country = val(phrase.countryname);
-// 		$("#country").append(country)
+// Translate API stuff:
+/*
+const axios = require("axios");
+var countryCode; //This needs to be a letter combo code for country of choice
 
-// 	}
-// }
-
-// $("#end").on("click", function(e) {
-// 	e.preventDefault();
-// 	let end = $("#end :selected").text();
-// 	console.log(end)
-// 	if (end === "China"){
-// 		console.log("you are almost chinese")
-// 		languagesdb.Phrase.findALL({
-// 			where:{
-// 				countryname:"China"
-// 			}
-// 		});
-// 			console.log(response);
-
-// 		}else if(end==="Korea"){
-// 			console.log("Lets go to korea")
-// 	}else{
-// 		console.log("Lets go to Russia")
-// 	}
-//   });
-
-// // Khalil
-// function GetSelectedEnd(end){
-// 	// let end = $("#end :selected").val();
-// 	switch(end){
-// 			case "PEK":
-// 					showChina();
-// 					break;
-// 			case "ICN":
-// 					showKorea();
-// 					break;
-// 			case "DME":
-// 					showRussia();
-// 					break;
-// 			default:
-// 					alert("Please choose a country")
-// 	}
-// };
-// function showChina (){
-// 	var query = connection.query("SELECT * FROM Phrase WHERE ? ", [
-// 			{
-// 			countryname: "China"
-// 	}
-// ], function(err, res){
-// 	if (err) throw err;
-// 	console.log(res);
-// })
-// 	console.log(query.sql)
-// }
+axios({
+    "method":"POST",
+    "url":"https://my-translator-pro.p.rapidapi.com/api/translate",
+    "headers":{
+    "content-type":"application/json",
+    "x-rapidapi-host":"my-translator-pro.p.rapidapi.com",
+    "x-rapidapi-key":"f0ce25292dmsha2e4b8e0953bbd1p1fed83jsnfd16ad1763cf",
+    "targetlanguage":"RU",
+    "accept":"application/json"
+    },"data":{
+    "sourceText":" Hello "
+    }
+    })
+    .then((response)=>{
+      console.log(response.data.Translated)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+*/
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,31 +1,24 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our Todo model
+// Requiring our phrase model
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // GET route for getting all of the todos
+  // GET route for getting all of the phrases
   app.get("/api/flights/:countryname", function(req, res) {
     // findAll returns all entries for a table when used with no options
     db.Phrase.findAll({
       where: { countryname: req.params.countryname }
     }).then(function(dbPhrase) {
-      // We have access to the todos as an argument inside of the callback function
+      // We have access to the phrases as an argument inside of the callback function
       res.json(dbPhrase);
     });
   });
 
-  // POST route for saving a new todo
+  // POST route for saving a new phrase
   app.post("/api/flights", function(req, res) {
     console.log(req.body);
-    
+
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
@@ -44,17 +37,16 @@ module.exports = function(app) {
       help: req.body.help,
       sorry: req.body.sorry
     })
-    
     .then(function(dbPhrase) {
-      // We have access to the new todo as an argument inside of the callback function
+      // We have access to the new phrase as an argument inside of the callback function
       res.json(dbPhrase);
     });
   });
 
-  // DELETE route for deleting todos. We can get the id of the todo we want to delete from
+  // DELETE route for deleting phrases. We can get the id of the phrase we want to delete from
   // req.params.id
   app.delete("/api/flights/:id", function(req, res) {});
 
-  // PUT route for updating flights. We can get the updated todo from req.body
+  // PUT route for updating phrases. We can get the updated phrase from req.body
   app.put("/api/flights", function(req, res) {});
 };
