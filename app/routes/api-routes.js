a45e9ee456bcc7365b265e4402a6db7df0ec9ffc
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-02-19 13:43:27 -0500

    I think it is fixed

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,28 +1,32 @@
-// Requiring our phrase model
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our languages model
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-
-  // GET route for getting all of the phrases
-  app.get("/api/flights/:countryname", function(req, res) {
-
+  // GET route for getting all of the Phrases
+  app.get("/:countryname", function(req, res) {
     // findAll returns all entries for a table when used with no options
     db.Phrase.findOne({
       where: { countryname: req.params.countryname }
     }).then(function(dbPhrase) {
-
-      // We have access to the phrases as an argument inside of the callback function
-      res.json(dbPhrase);
+      // We have access to the phrases in the table as an argument inside of the callback function
+      console.log(dbPhrase);
+      res.render("index", {phrases: dbPhrase});
     });
   });
 
-  // POST route for saving a new phrase
-
+  // POST route for saving a new country's phrases
   app.post("/api/flights", function(req, res) {
     console.log(req.body);
-
+    
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
@@ -41,18 +45,18 @@ module.exports = function(app) {
       help: req.body.help,
       sorry: req.body.sorry
     })
+    
     .then(function(dbPhrase) {
-      // We have access to the new phrase as an argument inside of the callback function
+      // We have access to the new language as an argument inside of the callback function
       res.json(dbPhrase);
     });
   });
 
-  // DELETE route for deleting phrases. We can get the id of the phrase we want to delete from
-  // req.params.id
+  // DELETE route for deleting Phrases. We can get the id of the phrase we want to delete from
+  // req.params.id//Tere is no function of deleting in this application at this point //
   app.delete("/:id", function(req, res) {});
 
-
-  // PUT route for updating phrases. We can get the updated phrase from req.body
-  app.put("/api/flights", function(req, res) {});
-
+  // PUT route for updating flights. We can get the updated phrase from req.body
+  app.put("/", function(req, res) {});
 };
+
